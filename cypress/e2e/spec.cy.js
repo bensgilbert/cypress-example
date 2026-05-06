61c39be6a6bdac95862ab2237b0f1c5534801b5f
Ben Gilbert <bensgilbert@outlook.com>
2026-05-06 02:01:59 +0100

    test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,4 +6,8 @@ describe('template spec', () => {
   it('Does not do much!', () => {
     expect(true).to.equal(true)
   })
+
+  it('Does not do much!', () => {
+    expect(true).to.equal(false)
+  })
 })
